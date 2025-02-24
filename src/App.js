1ae4b661fc69dfc2065ab08568203effe7a39faf
NguyenLe1104 <levannguyen2k44@gmail.com>
2025-02-24 15:27:19 +0700

    merge ctsp

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import Loaidat from "./pages/admin/Loaidat";
 import QuanLyBatdongsan from "./pages/admin/QuanLyBatdongsan";
 import MainLayout from "./layout/MainLayout";
 import Batdongsan from "./pages/user/Batdongsan";
+import ChiTietSanPham from "./pages/user/Chitietsanpham";
+
 function App() {
   const [showForm, setShowForm] = useState(null);
   const location = useLocation();
   const isAdminPage = location.pathname.startsWith("/admin");
-  const hiddenLayoutPath = ["/bat-dong-san"]
+
+  // Kiểm tra nếu đường dẫn là `/bat-dong-san` hoặc `/bat-dong-san/:id`
+  const isBatDongSanPage = location.pathname.startsWith("/bat-dong-san");
+
   return (
     <div className="App">
-      {!isAdminPage && !hiddenLayoutPath.includes(location.pathname) && (  // Ẩn MainLayout nếu trang nằm trong danh sách
+      {!isAdminPage && !isBatDongSanPage && (
         <MainLayout onFormChange={setShowForm} showForm={showForm} resetForm={() => setShowForm(null)} />
       )}
       <Routes>
@@ -21,6 +26,7 @@ function App() {
         <Route path="/admin/loaidat" element={<Loaidat />} />
         <Route path="/admin/batdongsan" element={<QuanLyBatdongsan />} />
         <Route path="/bat-dong-san" element={<Batdongsan />} />
+        <Route path="/bat-dong-san/:id" element={<ChiTietSanPham />} />
       </Routes>
     </div>
   );
