3dff21ffb2ee003057e05a6b585e247c2e4e5750
NguyenLe1104 <levannguyen2k44@gmail.com>
2025-02-24 14:49:36 +0700

    2402

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import AdminPage from "./pages/admin/AdminPage";
 import Loaidat from "./pages/admin/Loaidat";
-import Batdongsan from "./pages/admin/Batdongsan";
+import QuanLyBatdongsan from "./pages/admin/QuanLyBatdongsan";
 import MainLayout from "./layout/MainLayout";
-
+import Batdongsan from "./pages/user/Batdongsan";
 function App() {
   const [showForm, setShowForm] = useState(null);
   const location = useLocation();
   const isAdminPage = location.pathname.startsWith("/admin");
-
+  const hiddenLayoutPath = ["/bat-dong-san"]
   return (
     <div className="App">
-      {!isAdminPage && <MainLayout onFormChange={setShowForm} showForm={showForm} resetForm={() => setShowForm(null)} />}
-
+      {!isAdminPage && !hiddenLayoutPath.includes(location.pathname) && (  // Ẩn MainLayout nếu trang nằm trong danh sách
+        <MainLayout onFormChange={setShowForm} showForm={showForm} resetForm={() => setShowForm(null)} />
+      )}
       <Routes>
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/admin/loaidat" element={<Loaidat />} />
-        <Route path="/admin/batdongsan" element={<Batdongsan />} />
+        <Route path="/admin/batdongsan" element={<QuanLyBatdongsan />} />
+        <Route path="/bat-dong-san" element={<Batdongsan />} />
       </Routes>
     </div>
   );
